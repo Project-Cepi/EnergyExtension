382cd258e84dbdaf19b6fbc89a6488eca895ff05
LeoDog896 <LeoDog896@hotmail.com>
2021-04-02 09:23:05 -0400

    Dont send packets during init

--- src/main/kotlin/world/cepi/energy/EnergyExtension.kt
+++ src/main/kotlin/world/cepi/energy/EnergyExtension.kt
@@ -1,21 +1,28 @@
 package world.cepi.energy
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension;
 import net.minestom.server.utils.time.TimeUnit
 import world.cepi.energy.command.EnergyCommand
+import world.cepi.kstom.addEventCallback
 
 class EnergyExtension : Extension() {
 
     override fun initialize() {
         MinecraftServer.getConnectionManager().addPlayerInitialization { player ->
-            player.energy = player.maxEnergy
+
+            player.addEventCallback<PlayerSpawnEvent> {
+
+                player.energy = player.maxEnergy
+
+                MinecraftServer.getSchedulerManager().buildTask { player.energy += player.energyRegen }
+                    .repeat(player.energyRegenTimeout.toLong(), TimeUnit.SECOND)
+                    .schedule()
+
+            }
 
             player.addEventCallback(PlayerEnergyChangeEvent::class.java) { displayFood(it) }
-
-            MinecraftServer.getSchedulerManager().buildTask { player.energy += player.energyRegen }
-                .repeat(player.energyRegenTimeout.toLong(), TimeUnit.SECOND)
-                .schedule()
         }
 
         MinecraftServer.getCommandManager().register(EnergyCommand())
